59a7b932aa753b8d8f28b296be0286ca45ecf509
Vova <tsv19su@gmail.com>
2022-05-07 01:42:57 +0300

    Правки по оформлению

--- Async.py
+++ Async.py
@@ -10,7 +10,7 @@ import platform
 import os
 
 
-# ветка Developers на GitHub-е - правка tsv19su 09
+# ветка Developers на GitHub-е - моя правка 10
 myhostname = socket.gethostname()
 print(myhostname)
 myfqdn = socket.getfqdn(myhostname)
